50ee6eda6e831fda89e700693ec9ab7225f92402
Simon Danielsson <simon.daraf@gmail.com>
2026-04-06 09:55:58 +0200

    Add navigation service

---
+++ MVVM/Model/Services/NavigationService.cs
@@ -0,0 +1,20 @@
+﻿using AutomatedSoundtrackSystem.MVVM.View;
+using System.Windows;
+
+namespace AutomatedSoundtrackSystem.MVVM.Model.Services
+{
+    public class NavigationService
+    {
+        private Func<Window> openGroupWindow;
+
+        public NavigationService(Func<OpenGroupWindow> openGroupWindow)
+        {
+            this.openGroupWindow = openGroupWindow;
+        }
+
+        public Window OpenGroupWindow()
+        {
+            return openGroupWindow.Invoke();
+        }
+    }
+}

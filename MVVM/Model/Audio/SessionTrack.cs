3cfac51ea08f6a03077670a10e89e015da2d806b
Simon Danielsson <simon.daraf@gmail.com>
2026-04-14 19:33:36 +0200

    Add session track to later handle track change delegation

---
+++ MVVM/Model/Audio/SessionTrack.cs
@@ -0,0 +1,63 @@
+﻿using NAudio.Wave;
+
+namespace SimpleSoundtrackManager.MVVM.Model.Audio
+{
+    public class SessionTrack : ISampleProvider, IDisposable
+    {
+        public event EventHandler<float[]>? OnBufferProcessed;
+
+        private LoopableCachedAudio audio;
+        private LoopableCachedAudio? toReplace;
+
+        public WaveFormat WaveFormat => audio.WaveFormat;
+
+        public float Volume { get; set; }
+
+        public SessionTrack(LoopableCachedAudio audio)
+        {
+            this.audio = audio;
+        }
+
+        public void RequestReplacement(LoopableCachedAudio audio)
+        {
+            audio.Position = audio.StartPosition;
+            toReplace = audio;
+        }
+
+        public int Read(float[] buffer, int offset, int count)
+        {
+            int samplesRead = 0;
+            float[] copy = [];
+            if (toReplace is null)
+            {
+                samplesRead = audio.Read(buffer, offset, count);
+                copy = new float[samplesRead];
+                for (int i = 0; i < samplesRead; i++)
+                {
+                    buffer[i + offset] *= Volume;
+                    copy[i] = buffer[i + offset];
+                }
+            }
+            else
+            {
+                // Do stuff later
+                samplesRead = audio.Read(buffer, offset, count);
+                copy = new float[samplesRead];
+                for (int i = 0; i < samplesRead; i++)
+                {
+                    buffer[i + offset] *= Volume;
+                    copy[i] = buffer[i + offset];
+                }
+            }
+
+            Task.Run(() => OnBufferProcessed?.Invoke(this, copy));
+            return samplesRead;
+        }
+
+        public void Dispose()
+        {
+            audio.Dispose();
+            toReplace?.Dispose();
+        }
+    }
+}
